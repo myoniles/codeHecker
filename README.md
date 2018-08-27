e4864a31adcc0d8ce791e80beb5f244a88764237
Michael <BLMdnst8@gmail.com>
2018-08-27 11:53:26 -0400

    file type added

---
+++ README.md
@@ -0,0 +1,24 @@
+# Intro
+
+The CodeHecker is a project that I want to make my code, unsable but only barely cypherable.
+This was an idea that I had ever since my first semsester at Purdue and felt that its high time I pursue my dream of somehow making my code even less readable.
+
+# Current proposed workflow:
+
+- take in code file
+- Get file type to specify lanague-specific tokens
+- traverse line by line and do the following:
+	- remove lines with comments
+	- add declared variables to variable list
+	- replace variables names with their hashes
+- write to new file: "{filename}_new.{type}"
+- recompile
+- if stderr is written to, abort
+- else replace old file with new file
+
+# Options
+
+-h: help
+-t: format of argument. if none are specified, it will check the file ending
+-s: be silent
+-f: replace even if there are compile errors after hecking up
--- codeHecker.py
+++ codeHecker.py
@@ -1,34 +1,24 @@
 #!/usr/bin/env python3
-
-# Current proposed workflow:
-# take in code file
-# Get file type to specify lanague-specific tokens
-# traverse line by line and do the following:
-#		remove lines with comments
-#		add declared variables to variable list
-#		replace variables names with their hashes
-# write to new file: "{filename}_new.{type}"
-# recompile
-# if stderr is written to, abort
-# else replace old file with new file
-
-# options:
-# -h: help
-# -f: format of argument. if none are specified, it will check the file ending
-# -s: be silent
-# -f: replace even if there are compile errors after hecking up
-
 import sys
 import hashlib
 import getopt
 
-def detect_code_type():
+TYPE = ""
+VARIABLE = {}
+
+
+def detect_code_type(file_name):
+	global TYPE
+	file_name = file_name.split(".")
+	if file_name[1]:
+		TYPE = file_name[-1]
 	return
 
 def tokenize_line():
 	return
 
 def replace_variable():
+	hashlib.md5()
 	return
 
 def compile():
@@ -38,7 +28,9 @@ def abort():
 	return
 
 def __main__():
+	detect_code_type(sys.argv[1])
+	print(TYPE)
 	return
 
 if __name__ == "__main__":
-	main()
+	__main__()
